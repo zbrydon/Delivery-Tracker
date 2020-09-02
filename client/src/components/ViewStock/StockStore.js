b1242cc396885596b90cdf7e71ccb94fd97652fa
Anthony Papoutsis <anthony8121999@gmail.com>
2020-09-02 21:33:49 +1000

    Fixes

--- client/src/components/ViewStock/StockStore.js
+++ client/src/components/ViewStock/StockStore.js
@@ -4,33 +4,34 @@ import "./styling.css";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 
-const StockStore = () => {
-    const API_URL = process.env.REACT_APP_API_URL;
-    const token = localStorage.getItem('auth-token');
-    const history = useHistory();
-    const headers = {
-        'Authorization': token
-    };
-    axios.get(
-        `${API_URL}/viewStoreSOH`, { headers }
-    ).then(response => {
+// const StockStore = () => {
+//     const API_URL = process.env.REACT_APP_API_URL;
+//     const token = localStorage.getItem('auth-token');
+//     const history = useHistory();
+//     const headers = {
+//         'Authorization': token
+//     };
+//     axios.get(
+//         `${API_URL}/viewStoreSOH`, { headers }
+//     ).then(response => {
         
-        if (response.data.success) {
-            const frozen = response.data.SOH.frozen
-            const dairy = response.data.SOH.dairy
-            const meat = response.data.SOH.meat
-            const produce = response.data.SOH.produce
-            const ambient = response.data.SOH.ambient
-        }
-    }).catch(error => {
-        if (error.response.status === 406) {
-            //Display "Please refresh your session" here
-            //return history.push("/refresh");
-        } if (error.response.status === 403) {
-            //Display "Please login" here
-            history.push("/");
-        }
-    });
+//         if (response.data.success) {
+//             const frozen = response.data.SOH.frozen
+//             const dairy = response.data.SOH.dairy
+//             const meat = response.data.SOH.meat
+//             const produce = response.data.SOH.produce
+//             const ambient = response.data.SOH.ambient
+//         }
+//     }).catch(error => {
+//         if (error.response.status === 406) {
+//             //Display "Please refresh your session" here
+//             //return history.push("/refresh");
+//         } if (error.response.status === 403) {
+//             //Display "Please login" here
+//             history.push("/");
+//         }
+//     });
+// }
 
 class StockStore extends Component
 {
@@ -86,5 +87,5 @@ class StockStore extends Component
         )
     }
 }
-}
+
 export default StockStore
