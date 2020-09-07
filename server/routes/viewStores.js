88feeb43b2243e95b2a64a0c0bee9257cb4e32fb
zbrydon <bry0004@gmail.com>
2020-09-07 15:02:23 +1000

    Update viewStores.js

--- server/routes/viewStores.js
+++ server/routes/viewStores.js
@@ -4,36 +4,40 @@ const viewStoress = require('../services/viewStores');
 const viewStores = express.Router();
 
 /**
- * @api {get} /viewStoreSOH View Store SOH
- * @apiName View Store SOH
- * @apiGroup Store
+ * @api {get} /viewStores View Stores that have outstanding orders
+ * @apiName View Stores
+ * @apiGroup Warehouse
  *
  *
  * @apiSuccess {Boolean} success Request result
  * @apiSuccess {String} message Request status
- * @apiSuccess {Object} SOH The SOH from that store
+ * @apiSuccess {Object} stores List of store ids
  *
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
  *      {
- *           "success": true,
- *           "message": "SOH Displayed",
- *           "SOH": {
- *               "frozen": "6",
- *               "dairy": 0,
- *               "meat": 0,
- *               "produce": 0,
- *               "ambient": 0
- *           }
- *       }
+ *          "success": true,
+ *          "message": "Stores Displayed",
+ *          "stores": [
+ *              {
+ *                  "id": 11111
+ *              },
+ *              {
+ *                  "id": 11112
+ *              },
+ *              {
+ *                  "id": 11113
+ *              }
+ *          ]
+ *      }
  *
- * @apiError NotFound The store does not exist.
+ * @apiError NotFound No Stores have Ordered.
  *
  * @apiErrorExample Error-Response:
  *      HTTP/1.1 404 Not Found
  *      {
  *           "success": false,
- *           "message": "This store does not esist"
+ *           "message": "No Stores have ordered"
  *      }
  **/
 
