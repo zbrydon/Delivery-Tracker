3a6c7176708ce1b7c062afee32fbef244e8c1343
zbrydon <bry0004@gmail.com>
2020-09-10 19:24:45 +1000

    Update StoreView.js

--- client/src/components/StoreView/StoreView.js
+++ client/src/components/StoreView/StoreView.js
@@ -8,18 +8,32 @@ const StoreView = () => {
   const [store1, setStore1] = useState({});
   const [store2, setStore2] = useState({});
   const [store3, setStore3] = useState({});
-  const [store4, setStore4] = useState({});
+    const [store4, setStore4] = useState({});
 
+    let stores = [];
   useEffect(() => {
     const data = async () => {
       const API_URL = process.env.REACT_APP_API_URL;
       const token = localStorage.getItem("auth-token");
-      const headers = { authorization: token };
+        const headers = { authorization: token };
+
+
+        let store = {
+            id: null,
+            status: null
+        };
+        
 
       const result = await Axios.get(`${API_URL}/viewStores`, { headers });
         for (let i = 0; i < result.data.count; i++) {
             //render components dynamically here
+            store.id = result.data.stores[i].id;
+            store.status = "Requested";
+            stores[i] = store
+            //localStorage.setItem(`x${i}`, stores[i].id);
+            //localStorage.setItem(`z${i}`, stores[i].status);
         }
+        
 
       if (result.data.stores[0].id === 11111 && result.data.success === true) {
         setStore1({ reqStatus: "Requested", theme: "#FDC0C3" });
@@ -47,70 +61,89 @@ const StoreView = () => {
     };
     data();
   }, []);
+    return (
+        /*<div>
+            <NavBar />
+            <br></br>
+            <div className="card-container">
+                <div className="cards">
+                    {stores.map((store) => (
+                        <div className="card" style={{ background: "#FDC0C3" }} >
+                            <h3 key={store}>{store.id}</h3>
+                            <span className="status" key={store}>{store.status}</span>
+                            <a href={"/warehouseDB"}>
+                                <button>Details</button>
+                            </a>
+                        </div >
+                        ))}
 
-  return (
-    <div>
-      <NavBar />
-      <br></br>
-      <div className="card-container">
-        <div className="cards">
-          <div
-            className="card"
-            style={{
-              background:
-                store1.reqStatus === "Requested" ? store1.theme : "c9deff",
-            }}
-          >
-            <h3>Store 1</h3>
-            <span className="status">{store1.reqStatus}</span>
-            <a href={"/warehouseDB"}>
-              <button>Details</button>
-            </a>
-          </div>
-          <div className="card" style={{ background: store2.theme }}>
-            <span className="card-title">
-              <h3>Store 2</h3>
-            </span>
-            <span className="status">{store2.reqStatus}</span>
-            <a href={"/warehouseDB"}>
-              <button>Details</button>
-            </a>
-          </div>
+                </div>
+            </div>
+        </div>*/
+        <div>
+            <NavBar />
+            <br></br>
+            <div className="card-container">
+                <div className="cards">
+                    <div
+                        className="card"
+                        style={{
+                            background:
+                                store1.reqStatus === "Requested" ? store1.theme : "c9deff",
+                        }}
+                    >
+                        <h3>Store 1</h3>
+                        <span className="status">{store1.reqStatus}</span>
+                        <a href={"/warehouseDB"}>
+                            <button>Details</button>
+                        </a>
+                    </div>
+                    <div className="card" style={{ background: store2.theme }}>
+                        <span className="card-title">
+                            <h3>Store 2</h3>
+                        </span>
+                        <span className="status">{store2.reqStatus}</span>
+                        <a href={"/warehouseDB"}>
+                            <button>Details</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-container">
+                <div className="cards">
+                    <div className="card" style={{ background: store3.theme }}>
+                        <h3>Store 3</h3>
+                        <span className="status">{store3.reqStatus}</span>
+                        <a href={"/warehouseDB"}>
+                            <button>Details</button>
+                        </a>
+                    </div>
+
+                    <div
+                        className="card"
+                        style={{
+                            background:
+                                store4.reqStatus === "Requested" ? store4.theme : "#c9deff",
+                        }}
+                    >
+                        <h3>Store 4</h3>
+
+                        <span className="status">
+                            {store4.reqStatus === "No Request"
+                                ? setStore4({ reqStatus: "No Request" })
+                                : store4.reqStatus}
+                        </span>
+
+                        <a href={"/warehouseDB"}>
+                            <button>Details</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div className="card-container">
-        <div className="cards">
-          <div className="card" style={{ background: store3.theme }}>
-            <h3>Store 3</h3>
-            <span className="status">{store3.reqStatus}</span>
-            <a href={"/warehouseDB"}>
-              <button>Details</button>
-            </a>
-          </div>
-
-          <div
-            className="card"
-            style={{
-              background:
-                store4.reqStatus === "Requested" ? store4.theme : "#c9deff",
-            }}
-          >
-            <h3>Store 4</h3>
-
-            <span className="status">
-              {store4.reqStatus === "No Request"
-                ? setStore4({ reqStatus: "No Request" })
-                : store4.reqStatus}
-            </span>
-
-            <a href={"/warehouseDB"}>
-              <button>Details</button>
-            </a>
-          </div>
-        </div>
-      </div>
-      </div>
+   
   );
 };
 
