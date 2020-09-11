89c5b1d05ae43e1fcde0650cf288486055dcaad6
huynkhai0505 <huynhkhai0505@gmail.com>
2020-09-11 15:43:03 +0700

    Updating Everything

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     const loginRes = await axios.post(`${API_URL}/login`, loginUser);
 
     if (loginRes.data.type === "warehouse") {
-        history.push("/WarehouseDB");
+        history.push("/storeView");
       localStorage.setItem(
         "auth-token",
         "Bearer " + loginRes.data.tokens.token
