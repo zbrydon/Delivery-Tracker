469f271b85b84c1e115e180adf25ef4759994c4e
Trung Nguyen <trungnguyen@Johns.local>
2020-09-12 09:13:20 +0700

    Resolve conflicts of FronttEnd

--- client/src/components/WarehouseDB/WarehouseDB.js
+++ client/src/components/WarehouseDB/WarehouseDB.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../Tools/WarehouseNavbar";
 import "../WarehouseDB/WarehouseDB.Modules.css";
 import { useHistory, useLocation } from "react-router-dom";
-import Moment from 'react-moment';
+import Moment from "react-moment";
 
 const WarehouseDB = () => {
-
   const [orders, setOrders] = useState([]);
   const [ordersView, setOrdersView] = useState([]);
-  const [productType, setproductType] = useState('');
+  const [productType, setproductType] = useState("");
   const query = useQuery();
 
   useEffect(() => {
-      const API_URL = process.env.REACT_APP_API_URL;
-      const token = localStorage.getItem("auth-token");
-      const headers = { authorization: token };
-      const param = { storeId : query.get('storeId'), warehouseId: 1111};
+    const API_URL = process.env.REACT_APP_API_URL;
+    const token = localStorage.getItem("auth-token");
+    const headers = { authorization: token };
+    const param = { storeId: query.get("storeId"), warehouseId: 1111 };
 
-      axios.get(`${API_URL}/viewWarehouseOrders`, { 
+    axios
+      .get(`${API_URL}/viewWarehouseOrders`, {
         headers: headers,
-        params : param
-       }).then(function (response) {
+        params: param,
+      })
+      .then(function (response) {
         let data = response.data;
         setOrders(data.orders);
         setOrdersView(data.orders);
@@ -29,38 +30,41 @@ const WarehouseDB = () => {
       .catch(function (error) {
         console.log(error);
       });
+  }, []);
 
+  function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
 
-    }, []);
-
-    function useQuery() {
-      return new URLSearchParams(useLocation().search);
+  function searchOrder(e) {
+    let keySearch = e.target.value;
+    if (keySearch == "") {
+      setOrders(ordersView);
+      return;
     }
+    keySearch = parseInt(keySearch);
+    let ordersTemp = ordersView.filter((x) => x.orderId === keySearch);
+    setOrders(ordersTemp);
+  }
 
-    function searchOrder(e) {
-      let keySearch = e.target.value;
-      if(keySearch == '') {
-        setOrders(ordersView);
-        return;
-      }
-      keySearch = parseInt(keySearch);
-      let ordersTemp = ordersView.filter(x => x.orderId === keySearch);
-      setOrders(ordersTemp);
-    }
+  function selecteRow(index) {
+    let order = ordersView[index];
+    let productType = order.productType;
+    setproductType(productType);
+  }
 
-    function selecteRow(index) {
-      let order = ordersView[index];
-      let productType = order.productType;
-      setproductType(productType);
-    }
-
-    return (
+  return (
     <>
       <NavBar />
-      <br/>
+      <br />
       <div className="wrap">
         <div className="search">
-          <input type="text" className="searchID" placeholder="Deliver ID" onChange={searchOrder}/>
+          <input
+            type="text"
+            className="searchID"
+            placeholder="Deliver ID"
+            onChange={searchOrder}
+          />
           <button type="submit" className="searchButton">
             <i className="glyphicon glyphicon-search"></i>
           </button>
@@ -75,21 +79,20 @@ const WarehouseDB = () => {
               </tr>
             </thead>
             <tbody>
-            {orders.map((order, i) => {                  
-              // Return the element. Also pass key     
-              return (
-                <tr key={i} onClick={selecteRow.bind(this, i)}>
-                  <td>{order.orderId}</td>
-                  <td>
-                  <Moment format="DD/MM/YYYY" >
-                    {order.deliveryDateTime}
-                  </Moment>
+              {orders.map((order, i) => {
+                // Return the element. Also pass key
+                return (
+                  <tr key={i} onClick={selecteRow.bind(this, i)}>
+                    <td>{order.orderId}</td>
+                    <td>
+                      <Moment format="DD/MM/YYYY">
+                        {order.deliveryDateTime}
+                      </Moment>
                     </td>
-                  <td>{order.orderStatus}</td>
-                </tr>
-              ) 
-            })}
-
+                    <td>{order.orderStatus}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
@@ -97,17 +100,26 @@ const WarehouseDB = () => {
       <div className="wrap-table">
         <div className="card1">
           <h4>Order from Store</h4>
-          <button className="btn btn-sm" id={productType == 'frozen' ? 'button' : 'button-none'}>
+          <button
+            className="btn btn-sm"
+            id={productType == "frozen" ? "button" : "button-none"}
+          >
             <span className="glyphicon glyphicon-asterisk">
               <strong>Frozen</strong>
             </span>
           </button>
-          <button className="btn btn-sm" id={productType == 'normal' ? 'button' : 'button-none'}>
+          <button
+            className="btn btn-sm"
+            id={productType == "normal" ? "button" : "button-none"}
+          >
             <span className="glyphicon glyphicon-tree-deciduous">
               <strong>Normal</strong>
             </span>
           </button>
-          <button className="btn btn-sm" id={productType == 'dry' ? 'button' : 'button-none'}>
+          <button
+            className="btn btn-sm"
+            id={productType == "dry" ? "button" : "button-none"}
+          >
             <span className="glyphicon glyphicon-grain">
               <strong>Dry</strong>
             </span>
@@ -123,6 +135,5 @@ const WarehouseDB = () => {
     </>
   );
 };
-
 
 export default WarehouseDB;
--- server/services/viewWarehouseOrders.js
+++ server/services/viewWarehouseOrders.js
@@ -1,22 +1,22 @@
-const Order = require('../models/Order');
-const url = require('url');
+const Order = require("../models/Order");
+const url = require("url");
 
-function viewOrder(req , res, next) {
-    const query = url.parse(req.url,true).query;
-    const warehouseId = query.warehouseId;
-    const storeId = query.storeId;
-    
-    Order.find({ warehouseId: warehouseId, storeId: storeId }, (err, orders) => {
-        if (err) {
-            return res.status(400).send({
-                success: false,
-                message: err
-            });
-        } else {
-            res.locals.orders = orders;
-            next();
-        }
-    });
-};
+function viewOrder(req, res, next) {
+  const query = url.parse(req.url, true).query;
+  const warehouseId = query.warehouseId;
+  const storeId = query.storeId;
 
-module.exports = viewOrder;  
+  Order.find({ warehouseId: warehouseId, storeId: storeId }, (err, orders) => {
+    if (err) {
+      return res.status(400).send({
+        success: false,
+        message: err,
+      });
+    } else {
+      res.locals.orders = orders;
+      next();
+    }
+  });
+}
+
+module.exports = viewOrder;
