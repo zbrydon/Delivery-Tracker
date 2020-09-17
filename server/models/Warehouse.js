3b158033dd2fa8704b323b54ad0d5b4f3303eb68
zbrydon <bry0004@gmail.com>
2020-09-17 17:29:38 +1000

    Update Warehouse.js

--- server/models/Warehouse.js
+++ server/models/Warehouse.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 module.exports = mongoose.model('Warehouse', new mongoose.Schema({
     id: Number,
     password: String,
+    location: Object,
     SOH: Object,
     TEMP: Object
 }));
